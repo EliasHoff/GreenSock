6e330e4f602bf5f9f21bc3abed6571c2d80dd196
Elias Hoffmann <elias.feinstoff@gmail.com>
2021-06-20 20:18:55 +0200

    scrollTrigger erste Versuche

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
-gsap.from('.header', {duration: 1, y: '-100%', opacity: -1});
-gsap.from('.content', {duration: 1, y: '-100%', opacity: -1});
-gsap.to('.title', {duration: 1, delay: 1, fontSize: 50});
+gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll('.filler')[0].addEventListener('mouseenter', ()=>{
+const timeline = gsap.timeline({defaults: {duration: 1}});
+timeline
+    .from('.header', {y: '-100%', opacity: -1})
+    .from('.content', {y: '-100%', opacity: -1}, 0)
+    .to('.title', {fontSize: 50}, '<.3')
+;
+    
+
+gsap.from('.footer',{
+    scrollTrigger: {
+        trigger: '.footer',
+        toggleActions: 'restart reset restart reset'
+    },
+    duration: 1.5,
+    y: '-100%',
+    opacity: -1
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const filler = document.querySelector('.filler');
+filler.addEventListener('mouseenter', ()=>{
     gsap.to('.header', {duration: 0.5, opacity: 0});
 });
-document.querySelectorAll('.filler')[0].addEventListener('mouseleave', ()=>{
+filler.addEventListener('mouseleave', ()=>{
     gsap.to('.header', {duration: 0.5, opacity: 1});
+});
+
+document.querySelector('#reverseBtn').addEventListener('click', () => {  
+    timeline.timeScale(2);  
+    timeline.reverse();
 });
