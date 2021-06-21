f4af8ef1febe51f3c51ef5acc6e4de8ceee3503d
Elias <elias.feinstoff@gmail.com>
2021-06-21 09:03:18 +0200

    scroll for individual players / text animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 const timeline = gsap.timeline({defaults: {duration: 1}});
 timeline
@@ -6,20 +6,26 @@ timeline
     .from('.content', {y: '-100%', opacity: -1}, 0)
     .to('.title', {fontSize: 50}, '<.3')
 ;
-    
-
-gsap.from('.footer',{
-    scrollTrigger: {
-        trigger: '.footer',
-        toggleActions: 'restart reset restart reset',
-        start: 'center 95%',
-        end: 'center 5%',
-        markers: true
-    },
-    duration: 1.5,
-    y: '100%',
-    opacity: -1
+document.querySelector('#reverseBtn').addEventListener('click', () => {  
+    timeline.timeScale(2);  
+    timeline.reverse();
 });
+
+
+document.querySelectorAll('.footer').forEach(element => {
+    gsap.from(element, {
+        scrollTrigger: {
+            trigger: element,
+            toggleActions: 'restart complete none reset',
+            start: '25% 95%',
+            end: 'top 5%'
+        },
+        duration: .75,
+        y: '75%',
+        opacity: 0
+    });
+});
+
 
 
 
@@ -42,7 +48,22 @@ filler.addEventListener('mouseleave', ()=>{
     gsap.to('.header', {duration: 0.5, opacity: 1});
 });
 
-document.querySelector('#reverseBtn').addEventListener('click', () => {  
-    timeline.timeScale(2);  
-    timeline.reverse();
+const header = document.querySelector('#header');
+header.addEventListener('mouseenter', ()=>{
+    gsap.to('#header', {
+        text: {
+            value: "neuer Header!!!!!",
+            delimiter: '',
+            speed: 1
+        }
+    });
+});
+header.addEventListener('mouseleave', ()=>{
+    gsap.to('#header', {
+        text: {
+            value: "header text",
+            delimiter: '',
+            speed: 1
+        }
+    });
 });
